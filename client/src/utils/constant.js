a439a8a503af9b079b2a413448dfbd11b0685ea6
Khoa <nguyenvietkhoa12031993@gmail.com>
2021-07-14 14:54:37 +0900

    done

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,7 @@
 const apiUrl =
-  process.env.NODE_ENV !== "production" ? "http://localhost:3300/api" : "someDeployUrl";
+  process.env.NODE_ENV !== "production"
+    ? "https://fierce-citadel-75259.herokuapp.com/api"
+    : "someDeployUrl";
 const LOCAL_STORAGE_TOKEN = "token";
 const LOCAL_STORAGE_ROLE = "role";
 
